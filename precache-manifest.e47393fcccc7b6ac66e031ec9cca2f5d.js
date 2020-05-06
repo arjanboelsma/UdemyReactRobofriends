2e4f1bfc8128f0634bbc9538cc691d0d5d060adb
arjanboelsma <arjan.boelsma@dynalinq.eu>
2020-05-06 15:54:29 +0200

    Updates

---
+++ precache-manifest.e47393fcccc7b6ac66e031ec9cca2f5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40bc1e9b30acc08e5f26885d03b8ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "a29614e43e2bd053388b",
+    "url": "/static/css/main.9f3d9801.chunk.css"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29614e43e2bd053388b",
+    "url": "/static/js/main.46148562.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
